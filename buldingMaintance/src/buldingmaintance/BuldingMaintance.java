a26e890503de4622e9673cd1b0602689a9f0cbe0
mishpuha <mishpuha@mishpuha-PC>
2016-02-04 14:00:22 +0200

    ok

--- buldingMaintance/src/buldingmaintance/BuldingMaintance.java
+++ buldingMaintance/src/buldingmaintance/BuldingMaintance.java
@@ -14,6 +14,7 @@ public class BuldingMaintance {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("wasuppp????");
+
+        
     }
 }
