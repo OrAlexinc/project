ddcee2f5c42a03027ba777a074c79ceb2b13683a
alexanderCodes <massdigger@gmail.com>
2016-02-21 19:26:55 +0200

    Merge origin/master
    Conflicts:
    	buldingMaintance/src/buldingmaintance/Payment.java

--- buldingMaintance/src/buldingmaintance/ExternalWorker.java
+++ buldingMaintance/src/buldingmaintance/ExternalWorker.java
@@ -68,62 +68,6 @@ public class ExternalWorker {
         this.photo = photo;
     }
 
-    public class Feedback {
-
-        private int workerId;
-        private String feedback;
-        private int rating;
-        private String workDone;
-        private float priceTaken;
-
-        public Feedback(int workerId, String feedback, int rating, String workDone, float priceTaken) {
-            this.workerId = workerId;
-            this.feedback = feedback;
-            this.rating = rating;
-            this.workDone = workDone;
-            this.priceTaken = priceTaken;
-        }
-
-        public int getWorkerId() {
-            return workerId;
-        }
-
-        public void setWorkerId(int workerId) {
-            this.workerId = workerId;
-        }
-
-        public String getFeedback() {
-            return feedback;
-        }
-
-        public void setFeedback(String feedback) {
-            this.feedback = feedback;
-        }
-
-        public int getRating() {
-            return rating;
-        }
-
-        public void setRating(int rating) {
-            this.rating = rating;
-        }
-
-        public String getWorkDone() {
-            return workDone;
-        }
-
-        public void setWorkDone(String workDone) {
-            this.workDone = workDone;
-        }
-
-        public float getPriceTaken() {
-            return priceTaken;
-        }
-
-        public void setPriceTaken(float priceTaken) {
-            this.priceTaken = priceTaken;
-        }
-
-    }
+    
 
 }
---
+++ buldingMaintance/src/buldingmaintance/Feedback.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buldingmaintance;
+
+/**
+ *
+ * @author prime
+ */
+public class Feedback {
+    private int workerId;
+        private String feedback;
+        private int rating;
+        private String workDone;
+        private float priceTaken;
+
+        public Feedback(int workerId, String feedback, int rating, String workDone, float priceTaken) {
+            this.workerId = workerId;
+            this.feedback = feedback;
+            this.rating = rating;
+            this.workDone = workDone;
+            this.priceTaken = priceTaken;
+        }
+
+        public int getWorkerId() {
+            return workerId;
+        }
+
+        public void setWorkerId(int workerId) {
+            this.workerId = workerId;
+        }
+
+        public String getFeedback() {
+            return feedback;
+        }
+
+        public void setFeedback(String feedback) {
+            this.feedback = feedback;
+        }
+
+        public int getRating() {
+            return rating;
+        }
+
+        public void setRating(int rating) {
+            this.rating = rating;
+        }
+
+        public String getWorkDone() {
+            return workDone;
+        }
+
+        public void setWorkDone(String workDone) {
+            this.workDone = workDone;
+        }
+
+        public float getPriceTaken() {
+            return priceTaken;
+        }
+
+        public void setPriceTaken(float priceTaken) {
+            this.priceTaken = priceTaken;
+        }
+
+}
